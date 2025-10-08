cbe9d287651ae30eede51a5c9dd328f21e16b8fe
RustyOldProgrammer <theogrimmy+github@gmail.com>
2025-10-08 22:57:24 +0100

    Adding dependencies

---
+++ target/debug/deps/timer_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Theog\projects\github_rust\Lightweight-Timer\target\debug\deps\timer_cargo.exe: src\main.rs
+
+C:\Users\Theog\projects\github_rust\Lightweight-Timer\target\debug\deps\timer_cargo.d: src\main.rs
+
+src\main.rs:
