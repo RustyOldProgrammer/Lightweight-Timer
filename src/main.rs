0986662cb2c88366e24458445062db56b7d130d0
RustyOldProgrammer <theogrimmy+github@gmail.com>
2025-10-08 22:26:54 +0100

    Classifying GUI window

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,19 @@ use windows::win32::foundation::HWND;
 fn main() {
     unsafe{
         let mut msg = MSG::default(); 
-        
+
+        let class_name = widestring::U16CString::from_str("MyGuiContainer").unwrap();
+        let wnd_class = WNDCLASSW {
+            lpfnWndProc: Some(DefWindowProcW),
+            hInstance: h_instance,
+            lpszClassName: class_name.as_ptr(),
+            ..Default::default()
+        };
+
+        RegisterClassW(&wnd_class);
+
+        let hwnd = CreateWindowExW(
+
+        )
     }
 }
